8e3c8dab94cf258689fee7844687174056480b09
Macbook <macbook@Macbooks-MacBook-Pro.local>
2018-08-22 14:29:22 +0700

    fix email exist - final

--- config.php
+++ config.php
@@ -5,9 +5,6 @@ define (DB_DATABASE, "contact");
 define (DB_HOST, "localhost:8889");
 
 
-$mysqli = new mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_DATABASE);
-
-if ($mysqli->connect_error) {
-        die("Connection failed: " . $mysqli->connect_error);
-    }
+$mysqli = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_DATABASE)or die("Connection failed: " . $mysqli->connect_error);
+mysqli_query($conn,"SET NAMES 'UTF8'");
 ?>
--- signin_response.php
+++ signin_response.php
@@ -4,44 +4,46 @@ if($_POST["btn_submit"]) {
 
     extract($_POST);
 
-        if(!isset($_POST['name']) || !isset($_POST['name_hiragana']) || !isset($_POST['address_province'])|| !isset($_POST['address']) || !isset($_POST['email']) || !isset($_POST['password']) ) {
+    $name = $_POST['name'];
+    $name_hiragana = $_POST['name_hiragana'];
+    $address_province = $_POST['address_province'];
+    $address = $_POST['address'];
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+
+    //Kiểm tra điều kiện bắt buộc đối với các field không được bỏ trống
+        if ($name == "" || $name_hiragana == "" || $address_province == "" || $address == "" || $password == "" || $email == "") {
             die('We are sorry, please input to form');
-        }
-
-        $name = $_POST['name'];
-        $name_hiragana = $_POST['name_hiragana'];
-        $address_province = $_POST['address_province'];
-        $address = $_POST['address'];
-        $email = $_POST['email'];
-        $password = $_POST['password'];
-
-
-        // Kiểm tra email có bị trùng hay không
-        $sql = "SELECT * FROM member WHERE email = '$email'";
-          
-        // Thực thi câu truy vấn
-        $result = $mysqli->query($sql);
-          
-        // Nếu kết quả trả về lớn hơn 1 thì nghĩa là username hoặc email đã tồn tại trong CSDL
-        if (mysqli_num_rows($result) > 0)
-        {
-            // Sử dụng javascript để thông báo
-            echo '<script language="javascript">alert("Email exist"); window.location="signin.php";</script>';
-              
-            // Dừng chương trình
-            die ();
-        }
-        else {
-            // Ngược lại thì thêm bình thường
-            $sql2 = "INSERT INTO menber (name, name_hiragana, address_province, address, email, password) VALUES ('". $name . "','" . $name_hiragana ."','" . $address_province . "','" . $address ."','" . $email ."','" . md5($password) ."')";
-
-            $success = $mysqli->query($sql2);
-
-            if(!$success){
-                die("Couldn't enter data: ".$mysqli->error);
+        }else{
+            //thực hiện việc lưu trữ dữ liệu vào db
+            // Kiểm tra email có bị trùng hay không
+            echo "abc";
+            $sql = "SELECT * FROM menber WHERE email = '$email'";
+                  
+            // Thực thi câu truy vấn
+            $result = $mysqli->query($sql);
+            
+                  
+            // Nếu kết quả trả về lớn hơn 1 thì nghĩa là email đã tồn tại trong CSDL
+            if (mysqli_num_rows($result) > 0)
+            {
+                // Sử dụng javascript để thông báo
+                echo '<script language="javascript">alert("Email exist"); window.location="signin.php";</script>';
+                      
+                // Dừng chương trình
+                die ("Email exist");
             }
             else {
-                echo "会員登録ありがとうございました!";
+                // Ngược lại thì lưu trữ dữ liệu vào db
+                $sql2 = "INSERT INTO menber (name, name_hiragana, address_province, address, email, password) VALUES ('". $name . "','" . $name_hiragana ."','" . $address_province . "','" . $address ."','" . $email ."','" . md5($password) ."')";
+
+                $success = $mysqli->query($sql2);
+
+                if(!$success){
+                    die("Couldn't enter data: ".$mysqli->error);
+                }else {
+                    echo "会員登録ありがとうございました!";
+                }
             }
         }
 
